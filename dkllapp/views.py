9ba6725959c45a628e7a88691b25429c7f9acb20
louise-gautier <louise_gautier@orange.fr>
2022-01-05 15:06:02 +0100

    Avancement

--- dkllapp/views.py
+++ dkllapp/views.py
@@ -37,7 +37,7 @@ from .token import account_activation_token
 
 #v############################### FONCTIONS GENERIQUES #########################################
 def is_admin(user_id):
-    admins = [1, 2]
+    admins = [1, 2, 19]
     bool_admin = False
     if user_id in admins:
         bool_admin = True
