f659deca16627dd870bd33aa00f33609a6a01d2b
louise-gautier <louise_gautier@orange.fr>
2022-01-10 16:50:12 +0100

    Avancement

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="/static/dkllapp/img/favicon/splashscreens/iphone6_splash.png"/>
-<link rel="apple-touch-startup-image" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" href="/static/dkllapp/img/favicon/splashscreens/iphoneplus_splash.png"/>
-<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="/static/dkllapp/img/favicon/splashscreens/iphonex_splash.png"/>
-<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" href="/static/dkllapp/img/favicon/splashscreens/iphonexr_splash.png"/>
-<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" href="/static/dkllapp/img/favicon/splashscreens/iphonexsmax_splash.png"/>
-<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="/static/dkllapp/img/favicon/splashscreens/ipad_splash.png"/>
-<link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" href="/static/dkllapp/img/favicon/splashscreens/ipadpro1_splash.png"/>
-<link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" href="/static/dkllapp/img/favicon/splashscreens/ipadpro3_splash.png"/>
-<link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="/static/dkllapp/img/favicon/splashscreens/ipadpro2_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="640x1136" href="/static/dkllapp/img/favicon/splashscreens/iphone5_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="750x1334" href="/static/dkllapp/img/favicon/splashscreens/iphone6_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="1242x2208" href="/static/dkllapp/img/favicon/splashscreens/iphoneplus_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="1125x2436" href="/static/dkllapp/img/favicon/splashscreens/iphonex_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="828x1792" href="/static/dkllapp/img/favicon/splashscreens/iphonexr_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="1242x2688" href="/static/dkllapp/img/favicon/splashscreens/iphonexsmax_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="1536x2048" href="/static/dkllapp/img/favicon/splashscreens/ipad_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="1668x2224" href="/static/dkllapp/img/favicon/splashscreens/ipadpro1_splash.png"/>
+<link rel="apple-touch-startup-image" sizes="1668x2388" href="/static/dkllapp/img/favicon/splashscreens/ipadpro3_splash.png"/>
