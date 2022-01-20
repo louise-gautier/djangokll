d57bf75aa85cf8e46822b336fd789b3c5a51348d
louise-gautier <louise_gautier@orange.fr>
2022-01-20 09:54:06 +0100

    Avancement

---
+++ dkllapp/static/dkllapp/js/copy_ligue_id.js
@@ -0,0 +1,12 @@
+
+var elem = document.getElementsByClassName("copy_id_link");
+for (var i = 0; i < elem.length; i += 1) {
+    (function () {
+        var the_id = elem[i].name;
+        elem[i].addEventListener("click", function(){
+            input_objet = document.getElementById(the_id);
+            input_objet.select();
+            document.execCommand("copy");
+        }, false);
+    }());
+}
