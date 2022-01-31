213dd7e6b1222c74859798db6517614dcfcc42d3
louise-gautier <louise_gautier@orange.fr>
2022-01-31 14:22:07 +0100

    Avancement

--- dkllapp/static/dkllapp/js/url-uncollapse.js
+++ dkllapp/static/dkllapp/js/url-uncollapse.js
@@ -1,15 +1,10 @@
 jQuery(document).ready(function()
 {
-
     var url = document.location.toString();
-
-
     if ( url.match('#') ) {
       // exécute le gestionnaire
       var hash = url.split('#')[1]
       $('button[id='+ hash +']').addClass('active');
-      console.log('your message');
-      console.log($('button[id='+ hash +']').next('div'));
       $('button[id='+ hash +']').next('div').css('display', 'block')
      }
 })
